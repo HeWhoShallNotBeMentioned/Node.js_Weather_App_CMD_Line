8f51965033a668b4d2af8f0fdd458172a9a234ef
Chris Underwood <cunderwoodmn@gmail.com>
2017-04-02 23:26:46 -0700

    add code to parse the received data and to print it to the console.

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const http = require('http');
 const APIkey  = '4cc26b519f31b12f';
 
+function printMessage(city, temperature) {
+	const message = `${city} has a temperature of ${temperature}`;
+	console.log(message);
+}
+
 
 function getWeather(location) {
 	const request = http.get(`http://api.wunderground.com/api/${APIkey}/conditions/q/${location}.json`, 
@@ -10,7 +15,8 @@ function getWeather(location) {
 	           body += chunk;	
 	        });
 	         response.on('end', () => {
-	         	console.log(body);
+	         	const cityTemp = JSON.parse(body);
+	         	printMessage(cityTemp.current_observation.display_location.full, cityTemp.current_observation.temperature_string);
 	         });
 		}).on('error', (error) => {
 			  console.error(error.message);
