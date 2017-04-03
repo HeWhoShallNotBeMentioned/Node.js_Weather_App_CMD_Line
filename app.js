a70c778fd2f3a3a0d90a7bb7c8ac5fe9e807f02f
Chris Underwood <cunderwoodmn@gmail.com>
2017-04-02 23:08:02 -0700

    finished the data input with a basic data output of the entire object

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
-const https = require('https');
-const APIkey  = 4cc26b519f31b12f;
+const http = require('http');
+const APIkey  = '4cc26b519f31b12f';
 
 
 function getWeather(location) {
-	const request = https.get(`http://api.wunderground.com/api/${APIkey}conditions/q/OR/Bay_City.json`, (response)
-
-
-
+	const request = http.get(`http://api.wunderground.com/api/${APIkey}/conditions/q/${location}.json`, 
+		(response) => {
+			 let body = "";
+	         response.on('data', (chunk) => {
+	           body += chunk;	
+	        });
+	         response.on('end', () => {
+	         	console.log(body);
+	         });
+		}).on('error', (error) => {
+			  console.error(error.message);
+		});
 }
+
+
+const location = process.argv.slice(2).join("_").replace(' ', '_');
+getWeather(location);
